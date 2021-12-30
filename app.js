f62176bfa6f3ba5107c437b00fe04fb064853a80
khaled087 <khaled87bd@gmail.com>
2021-12-30 12:09:36 +0600

    project done

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-const btn = document.querySelector(".switch-btn")
+// const btn = document.querySelector(".switch-btn")
+// const video = document.querySelector(".video-container")
+
+// btn.addEventListener('click', function(){
+//     if(!btn.classList.contains("slide")){
+//         btn.classList.add("slide")
+//         video.pause();
+//     }else{
+//         btn.classList.remove("slide")
+//         video.play();
+//     }
+// });
+
+// preloader
+
+// const preloader = document.querySelector(".preloader");
+// window.addEventListener('load', function(){
+//     preloader.classList.add("hide-preloader")
+// })
+const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container")
 
 btn.addEventListener('click', function(){
     if(!btn.classList.contains("slide")){
         btn.classList.add("slide")
         video.pause();
-    }else{
+    }
+    else{
         btn.classList.remove("slide")
         video.play();
     }
 });
 
-// preloader
-
 const preloader = document.querySelector(".preloader");
+
 window.addEventListener('load', function(){
-    preloader.classList.add("hide-preloader")
-})
+    preloader.classList.add("hide-preloader");
+});
